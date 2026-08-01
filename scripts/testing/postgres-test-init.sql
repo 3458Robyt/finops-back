b61380a7af15824f86c495779a5c6461946cbe19
David <centeno70204@gmail.com>
2026-07-31 19:06:57 -0500

    test: add postgres role to integration fixture

--- scripts/testing/postgres-test-init.sql
+++ scripts/testing/postgres-test-init.sql
@@ -4,7 +4,7 @@ DO $$
 DECLARE
   role_name text;
 BEGIN
-  FOREACH role_name IN ARRAY ARRAY['anon', 'authenticated', 'service_role'] LOOP
+  FOREACH role_name IN ARRAY ARRAY['postgres', 'anon', 'authenticated', 'service_role'] LOOP
     IF NOT EXISTS (SELECT 1 FROM pg_catalog.pg_roles WHERE rolname = role_name) THEN
       EXECUTE pg_catalog.format('CREATE ROLE %I NOLOGIN', role_name);
     END IF;
